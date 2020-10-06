f3ab4895590a2a6867bb2592d6a4ea29786b977c
Paul Landers <paul@srednal.com>
2020-10-05 21:54:16 -0400

    Initial Commit

---
+++ XLineTool/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  XLineTool
+//
+//  Created by Paul Landers on 10/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
