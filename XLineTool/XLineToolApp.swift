d0621f234dffc1b8e3a75e91d8fc9f1876e13a9f
Paul Landers <paul@thelanders.family>
2026-09-19 14:47:59 -0400

    Upgrade to swiftui app lifecycle

---
+++ XLineTool/XLineToolApp.swift
@@ -0,0 +1,16 @@
+//
+//  XLineToolApp.swift
+//  XLineTool
+//
+
+import SwiftUI
+
+@main
+struct XLineToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .defaultSize(width: 480, height: 300)
+    }
+}
